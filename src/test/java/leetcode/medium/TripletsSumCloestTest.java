dcb023a8a6788e91ae24938050368b4eb5d2a0bc
meyeal@163.com <ydgithub0>
2018-07-02 00:36:36 +0800

    not solved

---
+++ src/test/java/leetcode/medium/TripletsSumCloestTest.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+import me.alfod.medium.TripletsSumCloest;
+import org.junit.Test;
+
+/**
+ * Created by Arvin Alfod on 2018/7/2.
+ */
+public class TripletsSumCloestTest {
+    private TripletsSumCloest tripletsSumCloest=new TripletsSumCloest();
+
+    @Test
+    public void test1(){
+
+    }
+}
